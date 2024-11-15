0ac7c677e319cd937f252e689ab2ff1af9a4ddc8
PauloSGM <meiotodo@hotmail.com>
2024-11-15 01:11:47 -0300

    regristando a primeira transação no banco e mostrando o nome

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,15 @@
-const transactionsPage = () => {
-    return <h1>trasactions page</h1>;
+import{db} from "../_lib/prisma"
+
+const TransactionsPage = async () => {
+  // Acessar as transações do banco
+  const transactions = await db.transaction.findMany({});
+    return (
+      <div>
+        {transactions.map((transaction) => (
+          <div key={transaction.id}>{transaction.name}</div>
+        ))}
+      </div>
+    );
   };
   
-  export default transactionsPage;
+  export default TransactionsPage ;
